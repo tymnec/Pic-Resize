8ba2cda7cede672a6e1d219fba875fc259f248b4
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-02-15 20:11:43 +0530

    Added new feature of constant ratio and some minor improvements

--- code/script.js
+++ code/script.js
@@ -3,6 +3,15 @@ const widthInput = document.querySelector('#width-input');
 const heightInput = document.querySelector('#height-input');
 const resizeButton = document.querySelector('#resize-button');
 const outputCanvas = document.querySelector('#output-canvas');
+const downloadButton = document.querySelector('#download-button');
+const increaseButton = document.querySelector('#increase-button');
+const decreaseButton = document.querySelector('#decrease-button');
+const smoothingCheckbox = document.querySelector('#smoothing-checkbox');
+
+let originalWidth = 0;
+let originalHeight = 0;
+let currentWidth = 0;
+let currentHeight = 0;
 
 imageInput.addEventListener('change', async (event) => {
   const image = event.target.files[0];
@@ -11,17 +20,41 @@ imageInput.addEventListener('change', async (event) => {
   outputCanvas.height = imageData.height;
   const ctx = outputCanvas.getContext('2d');
   ctx.putImageData(imageData, 0, 0);
+  originalWidth = imageData.width;
+  originalHeight = imageData.height;
+  currentWidth = originalWidth;
+  currentHeight = originalHeight;
+  widthInput.value = originalWidth;
+  heightInput.value = originalHeight;
 });
 
-resizeButton.addEventListener('click', () => {
+function handleResize() {
   const width = Number(widthInput.value);
   const height = Number(heightInput.value);
-  const imageData = resizeImage(outputCanvas, width, height);
+  currentWidth = width;
+  currentHeight = height;
+  const imageData = resizeImage(outputCanvas, width, height, smoothingCheckbox.checked);
   outputCanvas.width = width;
   outputCanvas.height = height;
   const ctx = outputCanvas.getContext('2d');
   ctx.putImageData(imageData, 0, 0);
-});
+}
+
+function handleIncrease() {
+  currentWidth *= 1.05;
+  currentHeight *= 1.05;
+  widthInput.value = Math.round(currentWidth);
+  heightInput.value = Math.round(currentHeight);
+  handleResize();
+}
+
+function handleDecrease() {
+  currentWidth *= 0.95;
+  currentHeight *= 0.95;
+  widthInput.value = Math.round(currentWidth);
+  heightInput.value = Math.round(currentHeight);
+  handleResize();
+}
 
 async function readImage(image) {
   return new Promise((resolve, reject) => {
@@ -43,20 +76,26 @@ async function readImage(image) {
   });
 }
 
-function resizeImage(canvas, width, height) {
+function resizeImage(canvas, width, height, smooth) {
   const imgData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
   const tmpCanvas = document.createElement('canvas');
   tmpCanvas.width = width;
   tmpCanvas.height = height;
-  tmpCanvas.getContext('2d').drawImage(canvas, 0, 0, width, height);
-  return tmpCanvas.getContext('2d').getImageData(0, 0, width, height);
+  const ctx = tmpCanvas.getContext('2d');
+  if (smooth) {
+    ctx.imageSmoothingEnabled = true;
+    ctx.drawImage(canvas, 0, 0, width, height);
+  } else {
+    ctx.imageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.drawImage(canvas, 0, 0, width, height);
+  }
+  return ctx.getImageData(0, 0, width, height);
 }
 
-const downloadButton = document.querySelector('#download-button');
+resizeButton.addEventListener('click', handleResize);
+increaseButton.addEventListener('click', handleIncrease);
+decreaseButton.addEventListener('click', handleDecrease);
 
-  downloadButton.addEventListener('click', () => {
-    const link = document.createElement('a');
-    link.download = 'resized-image.png';
-    link.href = outputCanvas.toDataURL();
-    link.click();
-  });
